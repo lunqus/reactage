28753f7aeb63e76e58fa36a03f36d5d123415eff
LUNKUS <sigitas@lunkus.com>
2019-02-07 06:48:42 +0200

    routing

---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Products extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello Guys</p>
+            </div>
+        );
+    }
+}
+
+export default Products;
