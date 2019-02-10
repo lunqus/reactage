65878810db89a7a634de37721cf5a435e2f9971f
LUNKUS <sigitas@lunkus.com>
2019-02-10 17:48:29 +0200

    css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import NavData from '../json/navData.json'
 import {NavLink} from "react-router-dom";
-import FooterData from "../json/footerData";
 
 const Navigation = () => {
 
     return (
 
         <header className="masthead mb-auto">
-            <div className="inner">
-                <h3 className="masthead-brand">VIZZY</h3>
+            <div className="inner row">
+
 
                 <nav className="nav nav-masthead justify-content-center">
+
+                    <div className="logo">
+                        <a href="#"><img src="../images/logo.svg" alt="vizzy"/></a>
+                    </div>
 
                     {NavData.map((json, index) => {
                         return <NavLink to={json.goto}> <a className="nav-link" href="/">{json.meniu}</a> </NavLink>
                     })}
 
+                    <div className="buttons">
+                        <button type="button" className="btn btn-primary">sign in</button>
+                        <button type="button" className="btn btn-primary">sign up</button>
+                    </div>
+
                 </nav>
+
 
             </div>
         </header>
