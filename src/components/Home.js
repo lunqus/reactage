8809f2983cc0d2f5566e36624b42cb88ffa71d16
LUNKUS <sigitas@lunkus.com>
2019-02-10 13:12:21 +0200

    home json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,35 @@
 import React from "react";
+import HomeData from "../json/homeData.json";
 
 const Home = () => {
 
     return (
+
         <div>
+            {HomeData.map((json, index) => {
+                return (
+                <div>
 
-            <main role="main" className="inner cover">
-                <h1 className="cover-heading">Create Stunning Visual Media</h1>
-                <p className="lead">Super simple tool to create trendy videos and pictures <br />  for your ads, social media, blogs and websites.<br />
-                    Make your project stand out!</p>
-                <p className="lead">
-                    <a href="#popup1" className="btn btn-lg btn-secondary">Create Design &nbsp;<i className="fas fa-arrow-right"></i></a>
-                </p>
-            </main>
-            <div id="popup1" className="overlay">
-                <div className="popup">
-                    <h2>Photoshop can fix anything</h2>
-                    <a className="close" href="#">&times;</a>
-                    <div className="content">
-                        Creative process is started ... work begins ...
+                    <main role="main" className="inner cover">
+                        <h1 className="cover-heading">{json.h1}</h1>
+                        <p className="lead">{json.p}</p>
+                        <p className="lead">
+                            <a href="#popup1" className="btn btn-lg btn-secondary">{json.button}<i className="fas fa-arrow-right"></i></a>
+                        </p>
+                    </main>
+                    <div id="popup1" className="overlay">
+                        <div className="popup">
+                            <h2>{json.h2}</h2>
+                            <a className="close" href="#">&times;</a>
+                            <div className="content">
+                                {json.content}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
+                </div>
+                )
+            })}
 
         </div>
     );
